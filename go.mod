7e32c517d77a726966deae7a1b37773390a30373
Tu Nguyen <im2nguyen@users.noreply.github.com>
2020-06-25 20:29:05 -0700

    convert to v2 boilerplate

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/hashicorp/terraform-provider-hashicups
+
+go 1.13
+
+require github.com/hashicorp/terraform-plugin-sdk/v2 v2.0.0-rc.2
--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/plugin"
-	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 
 	"github.com/hashicorp/terraform-provider-hashicups/hashicups"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: func() terraform.ResourceProvider {
+		ProviderFunc: func() *schema.Provider {
 			return hashicups.Provider()
 		},
 	})
