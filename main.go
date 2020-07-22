ec6462b6c46d92ffe270f2e139bff4b11eb19367
Tu Nguyen <im2nguyen@users.noreply.github.com>
2020-07-22 01:27:46 -0700

    reset boilerplate to learn tutorial

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 
-	"github.com/hashicorp/terraform-provider-hashicups/hashicups"
+	"terraform-provider-hashicups/hashicups"
 )
 
 func main() {
